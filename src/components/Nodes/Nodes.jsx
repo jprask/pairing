7a6ad771a4bd5dd8aa7e7295cda072431269fd90
jprask <jpraskys@gmail.com>
2019-05-08 18:13:40 -0300

    Add tests for Nodes component

--- src/components/Nodes/Nodes.jsx
+++ src/components/Nodes/Nodes.jsx
@@ -5,10 +5,11 @@ const Nodes = props => {
   const { nodes, selectNode, activeNode } = props
 
   return (
-    <div className="nodes-container">
+    <div className="nodes-container" data-testid="nodes-container">
       {nodes.map(
-        node => 
+        node =>
           <button 
+            data-testid={node}
             className={`node${node === activeNode ? "--active": ""}`}
             children={node} 
             key={node} 
